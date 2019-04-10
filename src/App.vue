80706c1360f506f198c28e322068839e678f151c
nakamura <nakamura@1-10.com>
2019-04-10 12:37:08 +0900

    update

--- src/App.vue
+++ src/App.vue
@@ -10,13 +10,14 @@ export default {
 }
 </script>
 
+<style lang="scss" module>
+@import "../node_modules/formula-css/develop/formula.scss";
+</style>
 <style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 100px 4%;
 }
 </style>
